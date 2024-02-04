f1134499104c000c307252280ed67439e3ac36fc
Noxlobin <aadrijamaitydps@gmail.com>
2024-02-04 14:17:29 +0530

    few changes

---
+++ src/components/BadgeItem.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+export default {
+  props: {
+    source: String,
+    alt: String
+  }
+}
+</script>
+
+<template>
+  <main>
+    <img :src="source" :alt="alt" class="rounded" />
+  </main>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import ImageItem from '@/components/ImageItem.vue'
+import BadgeItem from '@/components/BadgeItem.vue'
 import '@/assets/main.css'
 </script>
 
@@ -10,9 +11,9 @@ import '@/assets/main.css'
     <p>
       This is my website! My name is Noah and I am a <b>developer</b>! I specialize in frontend and
       backend development. My website has a blog and a page to know more about me. Check both of
-      them out! My tech stack is: Frontend tools, TypeScript, sqlite, c/c++, python, go, swift,
-      surrealdb and linux. This website is made in vue and typescript. It uses the vue router,
-      eslint, tailwind and prettier.
+      them out! My tech stack is: Frontend tools, TypeScript, sqlite, c/c++, python, go, supabase
+      and linux. This website is made in vue and typescript. It uses the vue router, eslint,
+      tailwind and prettier.
     </p>
     <p>
       Apart from that, these are my socials: <a href="https://twitter.com/Hash1616929">Twitter</a>,
@@ -20,6 +21,29 @@ import '@/assets/main.css'
       <a href="https://discord.gg/PwkGXb5R">Discord</a>.
     </p>
     <p>Thanks for visiting my little cove :)</p>
+    <h1 class="text-2xl font-semibold">Stack set</h1>
+    <div class="flex space-x-3 my-2">
+      <BadgeItem
+        source="https://img.shields.io/badge/javascript-yellow?style=for-the-badge&logo=javascript&logoColor=ffffff"
+        alt="JavaScript"
+      />
+      <BadgeItem
+        source="https://img.shields.io/badge/typescript-blue?style=for-the-badge&logo=typescript&logoColor=ffffff"
+        alt="TypeScript"
+      />
+      <BadgeItem
+        source="https://img.shields.io/badge/golang-blue?style=for-the-badge&logo=go&logoColor=ffffff"
+        alt="Golang"
+      />
+      <BadgeItem
+        source="https://img.shields.io/badge/c++-green?style=for-the-badge&logo=c%2B%2B&logoColor=ffffff"
+        alt="C++"
+      />
+      <BadgeItem
+        source="https://img.shields.io/badge/clang-purple?style=for-the-badge&logo=c&logoColor=ffffff"
+        alt="C"
+      />
+    </div>
   </main>
 </template>
 
