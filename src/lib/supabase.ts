4d549e6b12f8479b85709aeb51eceb85a7657b25
Noxlobin <aadrijamaitydps@gmail.com>
2024-02-05 14:53:41 +0530

    secured anon key + only works in production

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://hkbkjozcgjkchssrhvxs.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrYmtqb3pjZ2prY2hzc3JodnhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY5NTA0MDAsImV4cCI6MjAyMjUyNjQwMH0.Zpb4VvVoxD81ywJacquCVJ3Bhx0t5dZCPK3txJPrdEA'
+  process.env.ANON_KEY!
 )
