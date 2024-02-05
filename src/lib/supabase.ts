05692ea46445e81af14a589b7ca7cfb84e34153c
Noxlobin <aadrijamaitydps@gmail.com>
2024-02-05 15:05:10 +0530

    :arrow-up:

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import dotenv from 'dotenv'
-
-dotenv.config()
 
 export const supabase = createClient(
   'https://hkbkjozcgjkchssrhvxs.supabase.co',
