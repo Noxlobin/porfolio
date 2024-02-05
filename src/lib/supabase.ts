1616c42265d2d65b3fe23e7bc6baef165ab199b7
Noxlobin <aadrijamaitydps@gmail.com>
2024-02-05 15:03:37 +0530

    help me guys!

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,5 +5,5 @@ dotenv.config()
 
 export const supabase = createClient(
   'https://hkbkjozcgjkchssrhvxs.supabase.co',
-  process.env.ANON_KEY!
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrYmtqb3pjZ2prY2hzc3JodnhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY5NTA0MDAsImV4cCI6MjAyMjUyNjQwMH0.Zpb4VvVoxD81ywJacquCVJ3Bhx0t5dZCPK3txJPrdEA'
 )
