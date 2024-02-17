904037573028a835230998cbbaddd04942f75e1a
Noxlobin <aadrijamaitydps@gmail.com>
2024-02-17 14:40:24 +0530

    resort to the cloud

--- src/components/ImageItem.vue
+++ src/components/ImageItem.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <img
-      src="https://i.pinimg.com/originals/de/4c/5b/de4c5b0bcd9933fb757242fa0bef11e8.png"
+      src="https://files.catbox.moe/cca2a5.png"
       class="size-1/6 border-[0.56rem] border-gray-600/40 rounded-md my-4 filter"
     />
   </main>
